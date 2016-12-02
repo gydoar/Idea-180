7a5eee67cf8c23883aa6ace386d4968b88803c28
Andres Vega <gydoar@hotmail.com>
2016-12-01 22:01:30 -0500

    Ajustando cambios con Gean

--- footer.php
+++ footer.php
@@ -53,51 +53,6 @@
 	});
 </script>
 
-<!-- Hotjar Tracking Code -->
-<script>
-    (function(f,b,g){
-        var xo=g.prototype.open,xs=g.prototype.send,c;
-        f.hj=f.hj||function(){(f.hj.q=f.hj.q||[]).push(arguments)};
-        f._hjSettings={hjid:10154, hjsv:2};
-        function ls(){f.hj.documentHtml=b.documentElement.outerHTML;c=b.createElement("script");c.async=1;c.src="//static.hotjar.com/c/hotjar-10154.js?sv=2";b.getElementsByTagName("head")[0].appendChild(c);}
-        if(b.readyState==="interactive"||b.readyState==="complete"||b.readyState==="loaded"){ls();}else{if(b.addEventListener){b.addEventListener("DOMContentLoaded",ls,false);}}
-        if(!f._hjPlayback && b.addEventListener){
-            g.prototype.open=function(l,j,m,h,k){this._u=j;xo.call(this,l,j,m,h,k)};
-            g.prototype.send=function(e){var j=this;function h(){if(j.readyState===4){f.hj("_xhr",j._u,j.status,j.response)}}this.addEventListener("readystatechange",h,false);xs.call(this,e)};
-        }
-    })(window,document,window.XMLHttpRequest);
-</script>
-
-<!-- Start of Async HubSpot Analytics Code for WordPress v1.1.0 -->
-<script type="text/javascript">
-	var _hsq = _hsq || [];
-	_hsq.push(["setContentType", "standard-page"]);
-	(function(d,s,i,r) {
-	  if (d.getElementById(i)){return;}
-	  var n = d.createElement(s),e = document.getElementsByTagName(s)[0];
-	  n.id=i;n.src = '//js.hs-analytics.net/analytics/'+(Math.ceil(new Date()/r)*r)+'/1573132.js';
-	  e.parentNode.insertBefore(n, e);
-	})(document, "script", "hs-analytics", 300000);
-</script>
-<!-- End of Async HubSpot Analytics Code -->
-
-
-	<script>(function() {
-	var _fbq = window._fbq || (window._fbq = []);
-	if (!_fbq.loaded) {
-	var fbds = document.createElement('script');
-	fbds.async = true;
-	fbds.src = '//connect.facebook.net/en_US/fbds.js';
-	var s = document.getElementsByTagName('script')[0];
-	s.parentNode.insertBefore(fbds, s);
-	_fbq.loaded = true;
-	}
-	_fbq.push(['addPixelId', '1607066689552645']);
-	})();
-	window._fbq = window._fbq || [];
-	window._fbq.push(['track', 'PixelInitialized', {}]);
-	</script>
-	<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?id=1607066689552645&amp;ev=PixelInitialized" /></noscript>
 
 	
 	<!-- Google Tag Manager -->
